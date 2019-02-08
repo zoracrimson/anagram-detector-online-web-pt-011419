aea15bc6a25b96720b5a0e9a7d5a1cd103ccee08
Kat Maldon <katharinamaldon@gmail.com>
2019-02-08 18:25:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,4 +5,10 @@ class Anagram
   def initialize(word)
     @word = word
   end
+  
+  def match(array)
+    array.select do 
+      word.split("").sort == @word.split("").sort
+    end 
+  end 
 end 
