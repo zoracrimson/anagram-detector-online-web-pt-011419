300d3e0a4126904df994792366949467452c1629
Kat Maldon <katharinamaldon@gmail.com>
2019-02-08 18:29:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   def match(array)
     array.select do |word|
-    word.split("").sort == @word.split("").sort
+      word.split("").sort == @word.split("").sort
+    end 
   end 
-end 
 end 
